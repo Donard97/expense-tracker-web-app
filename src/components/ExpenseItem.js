774449d3f9d6ebeed686327d4733741c6a474df0
Donard97 <donardgolaj@gmail.com>
2021-10-30 01:13:35 +0200

    Add logic to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,25 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.97;
+const month = props.date.toLocaleString('en-US', {month: 'long'});
+const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+const year = props.date.getFullYear();
 
-  return <>
+  return (
+  <>
   <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
     </div>
   </div>
-  </>;
+  </>
+  );
 } 
 
 export default ExpenseItem;
