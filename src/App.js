28b9c117ac4ca8e7c9a51b5cf180b4e5264b1dd2
Donard97 <donardgolaj@gmail.com>
2021-10-30 01:33:05 +0200

    Add date function to different component on another file

--- src/App.js
+++ src/App.js
@@ -31,11 +31,13 @@ function App() {
     <ExpenseItem 
     title={expenses[0].title} 
     amount={expenses[0].amount} 
-    date={expenses[0].date}/>
+    date={expenses[0].date}
+    />
     <ExpenseItem 
     title={expenses[1].title} 
     amount={expenses[1].amount} 
-    date={expenses[1].date}/>
+    date={expenses[1].date}
+    />
     <ExpenseItem
     title={expenses[2].title} 
     amount={expenses[2].amount} 
