08c5ca9ada0eae3b1e274715d00b2713de856a61
Donard97 <donardgolaj@gmail.com>
2021-10-30 01:49:14 +0200

    Add expenses function to a different file and add style to it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem  from './components/ExpenseItem';
+import Expenses from "./components/expenses";
 
 function App() {
   const expenses = [
@@ -28,26 +28,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
     </div>
-    <ExpenseItem 
-    title={expenses[0].title} 
-    amount={expenses[0].amount} 
-    date={expenses[0].date}
-    />
-    <ExpenseItem 
-    title={expenses[1].title} 
-    amount={expenses[1].amount} 
-    date={expenses[1].date}
-    />
-    <ExpenseItem
-    title={expenses[2].title} 
-    amount={expenses[2].amount} 
-    date={expenses[2].date}
-    />
-    <ExpenseItem 
-    title={expenses[3].title} 
-    amount={expenses[3].amount} 
-    date={expenses[3].date}
-    />
+    <Expenses items={expenses} />
     </>
   );
 }
