f51702cf6ef673eeb91f9bddb7f64724bf9aeedc
Donard97 <donardgolaj@gmail.com>
2021-10-29 23:02:07 +0200

    Add file for expenses and pass data with props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.97;
+
   return <>
   <div className="expense-item">
-    <div>March 28th 2021</div>
+    <div>{props.date.toISOString()}</div>
     <div className="expense-item__description">
-      <h2 className="expense-item h2">Car Insurance</h2>
-      <div className="expense-item__price">$294.68</div>
+      <h2>{props.title}</h2>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   </div>
   </>;
-}
+} 
 
 export default ExpenseItem;
